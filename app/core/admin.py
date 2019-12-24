fe837c43f9274478dd16d3d715028c1fd05aa246
gilliano <gilliano.menezes@loadsmart.com>
2019-12-24 18:06:18 -0300

    Added endpoint for creating and authenticating users

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext as _
 
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
@@ -28,8 +29,6 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'password1', 'password2')
         }),
     )
-    
+
+
 admin.site.register(models.User, UserAdmin)
-
-
-
