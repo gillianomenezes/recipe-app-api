1a7cb4cddef220e725786f6656fdec6010a5362a
gilliano <gilliano.menezes@loadsmart.com>
2020-02-03 14:08:59 -0300

    Adjust file to attend flake8 rule and add Recipe model migration

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -63,6 +63,7 @@ class ModelTests(TestCase):
 
         self.assertEqual(str(ingredient), ingredient.name)
 
+
 def test_recipe_str(self):
     """Test the recipe string representation"""
     recipe = models.Recipe.objects.create(
